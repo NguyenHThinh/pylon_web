5621866a3b4a3c4e10e3c91c64adf7a4ae0ede63
gitThinh <nghthinh1998@gmail.com>
2025-02-04 20:16:12 +0700

    [responsive] hotfix active section in mobile nav

--- src/components/Header/NavMobile.vue
+++ src/components/Header/NavMobile.vue
@@ -1,35 +1,31 @@
 <template>
   <nav id="menu__mobile" class="nav__mobile">
-      <div class="nav__mobile__content px-3">
-        <ul class="nav__mobile--ul">
-          <li class="menu-item active"><a href="index.html">Home</a></li>
-          <li class="menu-item"><a href="#home-pylon">Pylon</a></li>
-          <li class="menu-item">
-            <a href="#home-power-up-players">Power Up Players</a>
-          </li>
-          <li class="menu-item"><a href="#home-form">Pylon Form</a></li>
-          <li class="menu-item">
-            <a href="#home-pylon-effect">Pylon Effect</a>
-          </li>
-          <li class="menu-item">
-            <a href="#home-earning-token">Earning Token</a>
-          </li>
-          <li class="menu-item"><a href="#home-multiverse">Multiverse</a></li>
-          <li class="menu-item">
-            <a href="#home-portal-key">Mutiserve Portal Key</a>
-          </li>
-          <li class="menu-item">
-            <a href="#home-minivault">Pylon’s Level & Minivault</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div class="nav__mobile__content px-3">
+      <ul class="nav__mobile--ul">
+        <li
+          :class="{ 'menu-item': true, 'active': currentActive === item.id }"
+          v-for="item in navList"
+          :key="item.id"
+          @click="$emit('selectNav', item.id)"
+        >
+          <a :href="'#' + item.id">
+            {{ item.label }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts" setup>
-
+defineProps({
+  navList: {
+    type: Array as PropType<{ id: string; label: string }[]>,
+    required: true,
+  },
+  currentActive: {
+    type: String,
+    required: true,
+  },
+});
 </script>
-
-<style>
-
-</style>
